42bdf740dd5916f98bc6a4b8d7bc7f77eb5c0b49
SAJITH P I <sajithpalamanna@gmail.com>
2021-11-29 13:04:26 +0530

    Updated the project file

--- Project-301.py
+++ Project-301.py
@@ -1,49 +1,65 @@
 from os import stat
 import datetime
+import time
+from colorama import Fore, init
+init()
 
 
 class Bank:
     service = '';
     # savings_amount = 10000;
-    def service_switch(self):
-        self.service = input("Choose Your bank option \n 1-Withdraw \n 2-Deposit \n 3-Balance-Enquiry \n")
-        if self.service == '1':
+    def service_switch(self):    
+            self.service = int(input(Fore.LIGHTYELLOW_EX + "Choose Your bank option \n 1-Withdraw \n 2-Deposit \n 3-Balance-Enquiry \n"))
+            if self.service == 1:
 
-            return 'Withdraw'
-        elif self.service == '2':
+                return 'Withdraw'
+            elif self.service == 2:
+                
+                return 'deposit '
             
-            return 'deposit'
-        
-        elif self.service == '3':
+            elif self.service == 3:
 
-            return 'b'
+                return 'balancecheck'
+        
+            else:
+                print(Fore.RED+"You choose wrong selection")
+     
       
-        else:
-            print("You choose wrong selection")
-            return 0    
 
     def withdraw(self,amount):
         if amount > 0:
             if self.savings_amount > amount :
+                print(Fore.LIGHTBLUE_EX +"Processing\n")
+                time.sleep(1.0)
                 self.savings_amount = self.savings_amount - amount
-                print("Withdrawel successfully completed")
+                print(Fore.GREEN +"\nWithdrawel successfully completed")
+                return amount
             
             else:
-                print("There is not much money in your account")
+                print(Fore.RED+"\nThere is not much money in your account")
+                
         else:
-            print("Enter a proper amount to withdraw")
+            print(Fore.RED+"\nEnter a Proper Amount to Withdraw")
+          
             
     def deposit(self,amount):
-        self.savings_amount = self.savings_amount + amount
-        print("Successfully Deposited to your account")       
+        if(amount > 0):
+            print(Fore.LIGHTBLUE_EX +"Processing\n")
+            time.sleep(1.0)
+            self.savings_amount = self.savings_amount + amount
+            print(Fore.GREEN +"Successfully Deposited to your account")      
+            return amount
+        else:
+            print(Fore.RED+"Enter a proper amount for deposit") 
          
     @property
     def currentBalance(self):
-        with open('bankBalanceStatement.text','r') as file:
-            print("Your Current Balance:",self.savings_amount)     
-            return self.savings_amount
+
         
-    
+                print(Fore.LIGHTBLUE_EX + "\nYour Current Balance:",self.savings_amount)     
+                return self.savings_amount
+        
+         
     def ReadStatement(self):
         with open("bankBalanceStatement.text","r") as file:
           statement = file.readlines()
@@ -51,79 +67,89 @@ class Bank:
               if "Current" in balanceSheet:
                 number = balanceSheet.split()
                 self.savings_amount = float(number[-1])
-                # print(number[-1])    
-        # print("Current Balance: ",self.savings_amount)
-            
+
+              else:
+                  self.savings_amount = 0.00
+                  
 
 
-    def printTransaction(self):
+    def printTransaction(self,serviceType,amount):
     
-        balance = [self.savings_amount,]
+     
         current_time = datetime.datetime.now()
-        with open('bankBalanceStatement.text','w') as file:
-            file.write("\t\t\tService Bank\t\t\n")
-            file.write(f"{current_time.hour}:{current_time.minute}:{current_time.second}\t\t\t\t\t\t{current_time.day}/{current_time.month}/{current_time.year}\n\n")
-
-            file.write(f"Current Balance: {self.savings_amount}")
-            #   file.write(self.savings_amount)
+        with open('bankBalanceStatement.text','a') as file:
+            file.write(f"\n{current_time.day}/{current_time.month}/{current_time.year}\t\t\t{current_time.hour}:{current_time.minute}:{current_time.second} ")       
+            file.write(f"\t\t\t{serviceType}\t\t{amount}\t\t\tCurrent Balance: {self.savings_amount}\n")
+          
 
 
-print("Welcome to Our Bank")
+print(Fore.MAGENTA +"\t\tWelcome to Our Bank")
 banking = True
 while(banking != False):
     try:
-        pin = int(input("Enter Your 4 digit pin Number: "))
+        pin = int(input(Fore.BLUE +"\nEnter Your 4 digit pin Number: "))
         if len(str(pin)) != 4:
             raise TypeError
-        bank = Bank()
-        service = bank.service_switch()
-        bank.ReadStatement()
-        print("Service option ",service)
-        if service.lower() == 'withdraw':
+        if(pin == 1234):
+            bank = Bank()
+            service = bank.service_switch()
+            service = service.upper()
+            bank.ReadStatement()
+            print(Fore.WHITE +"Service option ",service)
+            if service.lower() == 'withdraw':
+                
+                try:
+                    amount = float(input(Fore.LIGHTMAGENTA_EX + "Enter the amount for the withdrawel: "))
+                    amount = bank.withdraw(amount)
+                    savings = bank.currentBalance
+                    bank.printTransaction(service,amount)
+                except  ValueError:
+                    print(Fore.RED + "Only Numbers are alloweded ")   
+
+                except Exception as e:
+                    print(Fore.RED +"Exception caught")
+                    print(type(e))
+                    
+            if service.lower() == 'deposit ':
+                amount = float(input(Fore.LIGHTMAGENTA_EX +"Enter the amount:\n"))
+                amount = bank.deposit(amount)     
+                bank.printTransaction(service,amount) 
+                bank.currentBalance  
+                    
             
-            try:
-                amount = float(input("Enter the amount for the withdrawel: "))
-                bank.withdraw(amount)
-                savings = bank.currentBalance
-                bank.printTransaction()
-                # bill = input("Would you like to print the reciept of the transaction \n 1.Yes \n 2.No \n ")
-                # if bill in ['1', '2']:
-                #     if bill == '1':
-                #         bank.printTransaction()
-                #     else:
-                #         pass
-                # else:
-                #     print("Choose Proper Option")
-                
-            except Exception as e:
-                print("Exception caught")
-                print(type(e))
-                
-        if service.lower() == 'deposit':
-            amount = float(input("Enter the amount:\n"))
-            bank.deposit(amount)     
-            bank.printTransaction() 
-            bank.currentBalance  
-                
-        
-        if service.lower() == 'b':
-            print("Second")
-            bank.currentBalance
+            if service.lower() == 'balancecheck':
+                bank.currentBalance
+
+        else:
+            print(Fore.RED +"\n Enter Your Password Correctly To Use Our Atm Services")
             
         
     except TypeError:
-        print("Please Enter 4 digits number correctly")
+        print(Fore.RED +"Please Enter 4 digits number correctly")
     except ValueError:
-        print("Only 4 digit numbers are alloweded")
+        print(Fore.RED +"Only numbers are alloweded")
+    except AttributeError:
+          print(Fore.RED +"Select the option carefully")
     except Exception as e:
-        print("Exception was caught")
+        print(Fore.RED +"Exception was caught hetr")
         print(type(e))
-    
-    banking = int(input('Would you like to continue bank transaction?\n 1.Yes \n 2.No\n'))
-    if banking == 1:
-        banking = True
-        continue
+    try:
+        banking = int(input(Fore.CYAN +'\nWould you like to use our atm service again?\n 1.Yes \n 2.No\n'))
+    except ValueError:
+         print(Fore.RED+"You Choosed Improper Option \nLogin again to use our services")
+         break
+    if banking in [1 , 2]:
+
+        if banking == 1:
+            banking = True
+            continue
+        elif banking == 2:
+            print(Fore.LIGHTGREEN_EX + "ThankYou for Using our Services")
+            banking = False
+            break
     else:
-        banking = False
+        print(Fore.RED+"Don't use Improper Option \nLogin again to use our services")
         break
+
+       
 
